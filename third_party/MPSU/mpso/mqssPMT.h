24f7a9727c7a537a5feeac1bae1de91e8f6fea29
lx1234 <lx1234@buaa.edu.cn>
2024-04-04 20:40:29 +0800

    add MPSU

---
+++ third_party/MPSU/mpso/mqssPMT.h
@@ -0,0 +1,20 @@
+// COPYRIGHT 2023
+// Author: lx-1234
+
+#pragma once
+
+#include "ssVODM.h"
+#include <string>
+#include <vector>
+#include "../common/Defines.h"
+#include "../common/util.h"
+
+
+void mqssPMTSend(u32 numElements, LowMC &cipher,
+                 BitVector &out, std::vector<block> &okvs,
+                 Socket &chl, u32 numThreads, std::string triplePath = "");
+
+// we don't need cipher's key, only use round constants
+void mqssPMTRecv(std::vector<block> &set, LowMC &cipher,
+                 BitVector &out, Socket &chl,
+                 u32 numThreads, std::string triplePath = "");
